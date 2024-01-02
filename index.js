b4dcd13b51dd01bdc1e451ffe0b6ebd0f24e27e6
Wanessa Lopes <wanessaalopes@gmail.com>
2024-01-02 16:15:06 -0700

    Create express server

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
-const dataVideos = videos.json;
+const app = express();
+const dataVideos = "./data/videos.json";
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
 
 function readData() {
-    try {
-        const data =  fs.readFileSync(dataVideos);
-        return JSON.parse(data);
-    }
-    catch (error) {
-        return [];
-    }
+  try {
+    const data = fs.readFileSync(dataVideos);
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
 }
 
 function writeData() {
-    fs.writeFileSync(dataVideos, JSON.stringify);
+  fs.writeFileSync(dataVideos, JSON.stringify);
 }

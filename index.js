3abfffdde9b7613e0a5c127535c5796bd259277f
Wanessa Lopes <wanessaalopes@gmail.com>
2024-01-02 17:22:51 -0700

    Add /videos/:id on api

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
 });
 
+app.get('/videos', (req, res) => {
+   res.send(readData());
+});
+
+app.get('/videos/:id', (req, res) => {
+    const videos = readData();
+    const video = videos.find((video) => video.id === req.params.id);
+    if(video) {
+        res.json(video);
+    } else {
+        res.status(404).json({message:"Video not Found"});
+    }
+});
+
 function readData() {
   try {
     const data = fs.readFileSync(dataVideos);
